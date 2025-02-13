e9133c911c05f45435646f36153ec3f5ee4c3319
Irina Kuznetsova <kyznezova199217@gmail.com>
2025-02-13 15:23:04 +0300

    dev

--- src/test/java/adminpackage/GetAdmin.java
+++ src/test/java/adminpackage/GetAdmin.java
@@ -3,6 +3,7 @@ package adminpackage;
 import adminpackage.adminentity.AdminFromList;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.testng.annotations.Test;
 
 import static adminpackage.Auth.*;
 
@@ -10,11 +11,12 @@ import static helper.GetPost.getMethod;
 
 public class GetAdmin {
 
+    @Test
     public static void getAdmins() {
         String path = DEV_API_NODE + "/admin/";
         String responseString = getMethod(path);
 
-        System.out.println("Ответ на get: " + responseString);
+      //  System.out.println("Ответ на get: " + responseString);
         // attachJson(responseBody, GET_RESPONSE);
 
         JSONObject jsonObject = new JSONObject(responseString);
@@ -23,7 +25,20 @@ public class GetAdmin {
 
         for (int i = 0; i < adminArray.length(); i++) {
             AdminFromList adminFromList = new AdminFromList();
-            JSONObject dataObject = adminArray.getJSONObject(i);
+            JSONObject adminJson = adminArray.getJSONObject(i);
+
+            adminFromList.setId(adminJson.getInt("id"));
+            adminFromList.setEmail(adminJson.getString("email"));
+            // TODO: Заполнить остальное кроме роль
+            adminFromList.setStatus(adminJson.getString("status"));
+
+
+
+            System.out.println(adminFromList.getId());
+            System.out.println(adminFromList.getEmail());
+            System.out.println(adminFromList.getStatus());
+
+
         }
     }
 
@@ -31,7 +46,7 @@ public class GetAdmin {
         String path = DEV_API_NODE + "/admin/" + id;
         System.out.println(path);
         String responseString = getMethod(path);;
-        System.out.println(responseString);
+      //  System.out.println(responseString);
     }
 
 
