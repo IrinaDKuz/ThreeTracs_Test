ce66510fd22e33a3670cbdb14756410fb53227fd
roreljuk-QA <aostrikovmail@gmail.com>
2025-02-14 10:38:53 +0300

    mes

--- src/test/java/adminpackage/GetAdmin.java
+++ src/test/java/adminpackage/GetAdmin.java
@@ -13,30 +13,33 @@ import static adminpackage.Auth.*;
 import static helper.GetPost.getMethod;
 
 public class GetAdmin {
-    public static ArrayList<AdminFromList> ADMINS = new ArrayList<>();
+    public static ArrayList<AdminFromList> ADMINS = new ArrayList<>(); // ArrayList здесь хранятся все админы
 
     @Test
-    public static void getAdmins() {
-        String path = DEV_API_NODE + "/admin/";
-        String responseString = getMethod(path);
+    public static void getAdmins() { 
+        String path = DEV_API_NODE + "/admin/"; // урл для отправки запроса
+        String responseString = getMethod(path); // отправляем гет запрос
 
         //  System.out.println("Ответ на get: " + responseString);
         // attachJson(responseBody, GET_RESPONSE);
 
-        JSONObject jsonObject = new JSONObject(responseString);
-        JSONObject data = jsonObject.getJSONObject("data");
-        JSONArray adminArray = data.getJSONArray("admin");
+        JSONObject jsonObject = new JSONObject(responseString); // парсим поулченный json
+        JSONObject data = jsonObject.getJSONObject("data"); // берем data
+        JSONArray adminArray = data.getJSONArray("admin"); // берем массив админов
 
+// проходимся по всем админам
         for (int i = 0; i < adminArray.length(); i++) {
             AdminFromList adminFromList = new AdminFromList();
             JSONObject adminJson = adminArray.getJSONObject(i);
 
+            // из json заполняем наш лист с админами данными
             adminFromList.setId(adminJson.getInt("id"));
             adminFromList.setEmail(adminJson.getString("email"));
             adminFromList.setStatus(adminJson.getString("status"));
             adminFromList.setFirstName(adminJson.getString("firstName"));
             adminFromList.setSecondName(adminJson.getString("secondName"));
 
+            // указываем, что если в json ответе null, то в наш фал пишем null
             adminFromList.setSkype(adminJson.isNull("skype") ? null : adminJson.getString("skype"));
             adminFromList.setTelegram(adminJson.isNull("telegram") ? null : adminJson.getString("telegram"));
             adminFromList.setPhone(adminJson.isNull("phone") ? null : adminJson.getString("phone"));
@@ -46,21 +49,24 @@ public class GetAdmin {
             adminFromList.setUpdatedAt(adminJson.isNull("updatedAt") ? null : adminJson.getString("updatedAt"));
             adminFromList.setCreatedAt(adminJson.isNull("createdAt") ? null : adminJson.getString("createdAt"));
 
+            // ВОТ ТУТ ВОПРОС... ПОГУГЛИЛ ЧТО МЫ ТАК ДОБАВЛЯЕМ АДМИНА В СПИСОК, НО НЕ ПОНЯЛ
             ADMINS.add(adminFromList);
         }
     }
 
+    
     public static void getAdmin(int id) {
-        String path = DEV_API_NODE + "/admin/" + id;
-        System.out.println(path);
-        String responseString = getMethod(path);
+        String path = DEV_API_NODE + "/admin/" + id; // урл запроса инфы на админа с конкретным id
+        System.out.println(path); // выводим урл в терминал
+        String responseString = getMethod(path); // отправляем гет запрос
         ;
         //  System.out.println(responseString);
     }
 
     public static Integer getRandomEnableAdmin(ArrayList<AdminFromList> ADMINS) {
-        ArrayList<Integer> adminsIds = new ArrayList<>();
+        ArrayList<Integer> adminsIds = new ArrayList<>(); // создали список где будут храниться id админов
 
+        // проходимся по всем админам в списке
         for (int i = 0; i < ADMINS.size(); i++) {
 
             String status = ADMINS.get(i).getStatus();
@@ -69,14 +75,19 @@ public class GetAdmin {
                 adminsIds.add(ADMINS.get(i).getId());
             }
         }
-        // TODO: взять рандомный
+        // TODO: взять рандомный id админа
         Integer randomId = adminsIds.get(new Random().nextInt(adminsIds.size()));
         return randomId;
     }
 
 
     // TODO: ВОТ ТУТ СДЕЛАТЬ МЕТОД КОТОРЫЙ ВОЗВРАЩАЕТ РАНДОМНОЕ NAME АДМИНА
-    
+    // возвращаем строку (имя) случайного админа со статусом enable
+    public static String getRandomEnableAdminFirstName(ArrayList<AdminFromList> ADMINS) {
+        ArrayList<String> adminsFirstNames = new ArrayList<>();
+
+        //проходим по всем вдминам
+        for (string )
 
 
 }
